84bf5138423ff693f97e6c2b105e7585d0db99be
ThuF <jordan.p.pavlov@gmail.com>
2020-04-05 17:36:52 +0300

    Adds "api-modules" Extension

---
+++ io/extensions/modules.js
@@ -0,0 +1,21 @@
+exports.getContent = function() {
+	return [{
+		name: "io/v4/bytes",
+		description: "Bytes API"
+	}, {
+		name: "io/v4/files",
+		description: "Files API"
+	}, {
+		name: "io/v4/ftp",
+		description: "FTP API"
+	}, {
+		name: "io/v4/image",
+		description: "Image API"
+	}, {
+		name: "io/v4/streams",
+		description: "Streams API"
+	}, {
+		name: "io/v4/zip",
+		description: "ZIP API"
+	}];
+};
